4953c11d9c029af1639f908ecccda94a8fed6bed
minkyung <swkang1994@nate.com>
2020-07-09 15:53:40 +0900

    Create 2293_강민경.cpp

---
+++ 2020-07-10/2293_강민경.cpp
@@ -0,0 +1,25 @@
+// 2293 
+#include <stdio.h>
+using namespace std;
+
+int main() {
+
+    int i, j, n, k;
+    int arr[10001] = {0, };
+
+    scanf("%d %d", &n, &k);
+
+    int coins[n];
+
+    for (i = 0; i < n; i++) 
+        scanf("%d", &coins[i]);
+    
+    arr[0] = 1;
+    for (i = 0; i < n; i++) 
+        for (j = coins[i]; j <= k; j++) 
+            if (j - coins[i] >= 0) 
+                arr[j] += arr[j - coins[i]];
+
+    printf("%d\n", arr[k]);
+    return 0;
+}
