39e7a19f3e44af0333904e8fc35d25c5ccfbace6
Kimwoojae <50267433+kwj1270@users.noreply.github.com>
2020-07-30 17:31:54 +0900

    Update 2098_외판원순회_김우재(시간초과).cpp

--- 2020-08-01/2098_외판원순회_김우재(시간초과).cpp
+++ 2020-08-01/2098_외판원순회_김우재(시간초과).cpp
@@ -41,4 +41,51 @@ int main(){
     return 0;
 }
 // 문제 : 16 팩토리얼이 나오기에 시간이 오바되는건 당연**
+/*
 
+
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <queue>
+
+using namespace std;
+int n;
+vector<vector<long long>> map;
+vector<bool> passed;
+
+long long check(int count, int idx, long long length){
+    if(count == n) {
+        return length + map[idx][0];
+    }
+    long long ans = 9223372036854775807L;
+    for(int i=0; i < n ; i++){
+        if(passed[i] == true) continue;
+        passed[i] = true;
+        long long val = check(count+1, i, length+map[idx][i]);
+        ans = min(ans, val);
+        passed[i] = false;
+    }
+    return ans;
+}
+
+int main(){
+    cin >> n;
+    map.resize(n, vector<long long>(n));
+    passed.resize(n);
+    
+    for(int i=0; i < n; i++){
+        for(int j=0; j < n; j++){
+            cin >> map[i][j];
+        }
+    }
+    
+    passed[0] = true;
+    long long answer = check(1, 0, 0);
+    cout << answer << "\n";
+    return 0;
+}
+
+*/
