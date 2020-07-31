4085472e00958960ba63a398dc02012dd4e7f942
SangOk <sangok1993@naver.com>
2020-07-31 10:32:08 +0900

    Update 2098_외판원순회_이상옥(미완성).cpp

--- 2020-08-01/2098_외판원순회_이상옥(미완성).cpp
+++ 2020-08-01/2098_외판원순회_이상옥(미완성).cpp
@@ -1,5 +1,40 @@
 #include <stdio.h>
 #include <algorithm>
+
+#define MAX_V 20000000
+using namespace std;
+
+int W[16][16], N,city;
+
+int go_trip(int remain, int now){
+    if(remain==0){
+        if(W[now][city]==0) return MAX_V;
+        return W[now][city];
+    }
+    int answer=MAX_V;
+    for(int i=0;i<N;i++)
+        if(((remain>>i)%2==1)){
+            if(W[now][i]==0) continue;
+            answer=min(answer,W[now][i] + go_trip(remain-(1<<i),i));
+        }
+    return answer;
+}
+int main(){
+    int answer=MAX_V;
+    scanf("%d",&N);
+    for(int i=0;i<N;i++)
+        for(int j=0;j<N;j++)
+            scanf("%d",W[i]+j);
+    int remain=(1<<N)-1;
+    for(city=0;city<N;city++)
+        answer = min(answer, go_trip(remain-(1<<city), city)); //도시를 순회, 출발도시.
+    printf("%d\n",answer);
+    return 0;
+}
+
+//-------------------------------------
+#include <stdio.h>
+#include <algorithm>
 #include <vector>
 
 #define MAX_V 20000000
