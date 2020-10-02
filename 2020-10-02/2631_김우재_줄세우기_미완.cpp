49b65ad5442bd227db07135e1ecaf54aed683d7a
Kimwoojae <50267433+kwj1270@users.noreply.github.com>
2020-10-02 20:06:15 +0900

    Create 2631_김우재_줄세우기_미완.cpp

---
+++ 2020-10-02/2631_김우재_줄세우기_미완.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int n;
+int arr[200];
+int d[200];
+int main() {
+    cin >> n;
+    for(int i=0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    for(int i=0; i < n; i++){
+        d[i] = 0;
+        int standard = d[i];
+        for(int j=0; j < i; j++){
+            if (arr[j] > standard) {
+                d[i] = d[j] + 1;
+                standard = d[j];
+            }
+        }
+    }
+    
+    cout << d[n-1] << " ";
+    
+    
+    
+    return 0;
+}
