bb6a07864f88dd123604be0e428500a635b5f8b4
Kimwoojae <50267433+kwj1270@users.noreply.github.com>
2020-10-14 22:31:58 +0900

    Create 12872_김우재_플레이리스트_x.cpp

---
+++ 2020-10-16/12872_김우재_플레이리스트_x.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int n,m,p;
+long long mod = 1000000007;
+long long d[101][101];
+int main() {
+    cin >> n >> m >> p; // nmp 입력
+    d[0][0] = 1;
+    for(int i=1; i <= p; i++){ // i는 길이를 의미합니다.
+        for(int j=0; j <= n; j++){ // j는 x를 의미한다.
+        if(j != n) d[i][j + 1] = (d[i][j + 1] + d[i - 1][j] * (n - j)) % mod;
+        if(m < j) d[i][j] = (d[i][j] + d[i - 1][j] * (j - m)) % mod;
+        }
+    }
+    cout << d[p][n] << "\n";
+    
+    return 0;
+}
