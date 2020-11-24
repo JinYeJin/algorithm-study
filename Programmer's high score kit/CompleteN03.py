6298b8ca4b5b61b66bcef77b8da2c7a3b2db7f39
jake <74135462+Jake1152@users.noreply.github.com>
2020-11-24 16:31:36 +0900

    Create CompleteN03.py

---
+++ Programmer's high score kit/CompleteN03.py
@@ -0,0 +1,15 @@
+def solution(brown, yellow):
+	sum = brown/2 +2	#가로세로 합
+	area = brown+yellow
+	start =  brown//4+1# 가로길이 시작 숫자
+	end = brown//2+1
+	
+
+	while start<=end:
+		mid = (start+end)//2
+		if mid*(sum-mid) == area:
+			return [mid,sum-mid]
+		elif mid*(sum-mid) > area:
+			start = mid+1
+		else: #mid*(sum-mid) < area
+			end = mid-1
