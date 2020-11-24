0128111e07aeb82f50e43269a344d036322e0567
SangOk <sangok1993@naver.com>
2020-11-24 21:16:12 +0900

    Update 11054_이상옥_가장긴바이토닉부분수열.cpp

--- 2020-11-27/11054_이상옥_가장긴바이토닉부분수열.cpp
+++ 2020-11-27/11054_이상옥_가장긴바이토닉부분수열.cpp
@@ -17,3 +17,30 @@ int main(){
     cout<<m+1;
     return 0;
 }
+//////////
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, arr[1000],dp[1000]={0},b[1000]={0},n=0,answer=0;
+
+int main(){
+    cin>>N;
+    for(int i=0;i<N;i++) cin>>arr[i];
+    for(int i=0;i<N;i++){
+        auto it=lower_bound(dp,dp+n,arr[i]);
+        if(it==dp+n) dp[n++]=arr[i];
+        else *it=arr[i];
+        b[i]=it-dp+1;
+    }
+    n=0;
+    for(int i=N-1;i>=0;i--){
+        auto it=lower_bound(dp,dp+n,arr[i]);
+        if(it==dp+n) dp[n++]=arr[i];
+        else *it=arr[i];
+        answer=max(answer,b[i]+(int)(it-dp));
+    }
+    cout<<answer;
+    return 0;
+}
