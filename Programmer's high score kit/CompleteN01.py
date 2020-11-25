5b3af92c1504f32e250ed256f03a84c3846406c1
jake <74135462+Jake1152@users.noreply.github.com>
2020-11-25 19:26:07 +0900

    upload CompleteN01.py

---
+++ Programmer's high score kit/CompleteN01.py
@@ -0,0 +1,20 @@
+def solution(answers):
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+    result = []
+
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx%len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx%len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx%len(pattern3)]:
+            score[2] += 1
+
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+
+    return result
