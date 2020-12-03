4a23398d279ece1acc53a3595669706a321bc8d9
Min Young <32213721+griffinGC@users.noreply.github.com>
2020-12-04 00:52:35 +0900

    Update 1068_최민영_트리.py

--- 2020-12-04/1068_최민영_트리.py
+++ 2020-12-04/1068_최민영_트리.py
@@ -1,27 +1,24 @@
 import sys
 from collections import deque
-
+# 0번부터 n-1번까지 주어짐
 def solution():
     input = sys.stdin.readline
     node_number = int(input())
     node_list = list(map(int, input().split()))
     target = int(input())
 
-    root = 0
-    count = 0
-    if len(node_list) > 1:
-        root = node_list[1]
-    target_parent = -1
+    node_map = [[] for _ in range(node_number)]
+    count, target_parent = 0, 0
 
-    node_map = [[] for _ in range(node_number + root)]
     for index, value in enumerate(node_list):
         if value == -1:
             continue
-        node_map[value].append(index + root)
-        if index + root == target:
+        node_map[value].append(index)
+        if index == target:
             target_parent = value
 
-    visited = [ 0 for _ in range(node_number + root)]
+    # bfs
+    visited = [ 0 for _ in range(node_number)]
     q_list = deque()
     q_list.append(target)
     visited[target] = -1
@@ -32,23 +29,16 @@ def solution():
             if visited[node] == 0:
                 q_list.append(node)
 
-    result = list()
-
-    for i in range(root, len(node_map)):
+    for i in range(len(node_map)):
         if visited[i] == -1:
             node_map[i].append(-1)
         if len(node_map[i]) == 0:
             count += 1
-            result.append(i)
 
-
+    # target_parent의 노드가 target 뿐일때 고려
     if len(node_map[target_parent]) == 1 and node_map[target_parent][0] == target:
-        print("target_parent", node_map[target_parent])
         count += 1
-        result.append(target_parent)
-    print(visited)
-    print(node_map)
-    print(result)
+
     return count
 result = solution()
 print(result)
