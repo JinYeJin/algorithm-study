2831c89a36620c3efc2701fefa978e6f13e7cffc
Min Young <32213721+griffinGC@users.noreply.github.com>
2020-12-04 10:36:57 +0900

    Update 1475_최민영_방번호.py

--- 2020-12-04/1475_최민영_방번호.py
+++ 2020-12-04/1475_최민영_방번호.py
@@ -1,3 +1,4 @@
+import math
 def solution():
     num = input()
     num_count = [0 for _ in range(10)]
@@ -12,9 +13,7 @@ def solution():
         if max_val < val:
             max_val = val
 
-    six_or_nine = (num_count[6] + num_count[9]) // 2
-    if (num_count[6] + num_count[9]) % 2 != 0:
-        six_or_nine += 1
+    six_or_nine = math.ceil((num_count[6] + num_count[9]) / 2)
 
     max_val = max(six_or_nine, max_val)
     return max_val
