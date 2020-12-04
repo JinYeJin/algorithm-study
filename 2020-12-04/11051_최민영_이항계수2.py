1dff7de8901b83b1a2055712fe06672e86eb03f1
Min Young <32213721+griffinGC@users.noreply.github.com>
2020-12-04 12:42:03 +0900

    Update 11051_최민영_이항계수2.py

--- 2020-12-04/11051_최민영_이항계수2.py
+++ 2020-12-04/11051_최민영_이항계수2.py
@@ -1,3 +1,4 @@
+# 1
 import sys
 sys.setrecursionlimit(100000)
 def solution():
@@ -12,6 +13,23 @@ def solution():
 
     result = bino(n,k) % 10007
     return result
+
+result = solution()
+print(result)
+
+# 2
+def solution():
+    n, k = map(int, input().split())
+    dp = [[0 for _ in range(1001)] for _ in range(1001)]
+    for i in range(n+1):
+        dp[i][0] = 1
+    for j in range(k+1):
+        dp[j][j] = 1
+
+    for i in range(1, n+1):
+        for j in range(1, k+1):
+            dp[i][j] = (dp[i-1][j] + dp[i-1][j-1]) % 10007
+    return dp[n][k]
 
 result = solution()
 print(result)
