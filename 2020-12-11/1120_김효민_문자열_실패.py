17538b3aa2891b6df2f2706b52eebe76c31112a2
Hyomnie <35550051+Hyomnie@users.noreply.github.com>
2020-12-11 16:43:06 +0900

    Create 1120_김효민_문자열_실패.py

---
+++ 2020-12-11/1120_김효민_문자열_실패.py
@@ -0,0 +1,19 @@
+import sys
+
+input=sys.stdin.readline
+a,b = input().split()
+c1=0
+c2=0
+for i in range(len(a)):
+  if a[i]!=b[i]:
+    c1+=1
+    
+a=a[::-1]
+b=b[::-1]
+
+for i in range(len(a)):
+  if a[i]!=b[i]:
+    c2+=1
+    print(a[i])
+
+print(min(c1, c2))
