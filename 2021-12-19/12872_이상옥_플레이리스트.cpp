f6b1eb8d39f0c6691cf4aaea65bd47332839b3cf
SangOk <sangok1993@naver.com>
2021-02-15 16:13:15 +0900

    Create 12872_이상옥_플레이리스트.cpp

---
+++ 2021-12-19/12872_이상옥_플레이리스트.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string.h>
+
+#define mod 1000000007
+using namespace std;
+
+long dp[101][101];
+int N,M,P;
+long DFS(int n, int p){
+    if(dp[n][p]==-1){
+        dp[n][p]=(DFS(n-1,p-1)*(N-n+1))%mod;
+        if(n>M) (dp[n][p]+=DFS(n,p-1)*max(0,n-M))%=mod;
+    }
+    return dp[n][p];
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin>>N>>M>>P;
+    memset(dp,-1,sizeof(dp));
+    dp[0][0]=1;
+    for(int i=1;i<=N;i++)dp[i][0]=0;
+    for(int i=1;i<=P;i++)dp[0][i]=0;
+    
+    printf("%ld",DFS(N,P));
+    return 0;
+}
