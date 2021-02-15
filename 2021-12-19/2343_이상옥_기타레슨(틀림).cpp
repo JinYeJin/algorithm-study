5ca788c5ec6596cdf8ed53194dc26f8dc0590eed
SangOk <sangok1993@naver.com>
2021-02-15 16:24:05 +0900

    Create 2343_이상옥_기타레슨(틀림).cpp

---
+++ 2021-12-19/2343_이상옥_기타레슨(틀림).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int N,M,lesson[100000];
+int bisearch(int s, int e){
+    int m,k,blue,sum;
+    while(s+1<e){
+        m=s+e>>1;
+        k=0;
+        blue=0;
+        while(blue++<M && k<N){
+            sum=0;
+            while(k<N){
+                sum+=lesson[k];
+                if(sum>m){
+                    break;
+                }
+                k++;
+            }
+        }
+        if(k<N){
+            s=m;
+        }else{
+            e=m;
+        }
+    }
+    return e;
+}
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    cin>>N>>M;
+    for(int i=0;i<N;i++){
+        cin>>lesson[i];
+    }
+    cout<<bisearch(0,100000);
+    return 0;
+}
