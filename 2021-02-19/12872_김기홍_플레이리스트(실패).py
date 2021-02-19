2a719bfdbaf5fa717c064b60380c76258081bb64
Airvw <airvw14@gmail.com>
2021-02-19 16:38:43 +0900

    날짜 조정

--- 2021-02-19/12872_김기홍_플레이리스트(실패).py
+++ 2021-02-19/12872_김기홍_플레이리스트(실패).py
@@ -16,5 +16,26 @@ for i in range(p):
     answer *= false_cnt
     arr[i][0] = True
     arr[i][1] = m
-    
+
+print(answer)
+
+
+# ------------------------------
+
+n, m, p = map(int, input().split())
+
+cnt = [0] * n
+
+answer = 1
+
+for i in range(p):
+    false_cnt = 0
+    for j in range(n):
+        if cnt[j] == 0:
+            false_cnt += 1
+        if cnt[j] != 0:
+            cnt[j] -= 1
+    answer *= false_cnt
+    cnt[i % n] = m
+
 print(answer)
