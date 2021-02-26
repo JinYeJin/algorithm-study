ad4481f2e8f0fe85f9411ac5dd9d6ea653d1ea0b
SangOk <sangok1993@naver.com>
2021-02-26 16:35:59 +0900

    Create 1620_이상옥_나는야포켓몬마스터이상옥.cpp

---
+++ 2021-02-26/1620_이상옥_나는야포켓몬마스터이상옥.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+string dogam1[100000];
+pair<string,int> dogam2[100000];
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    int N,M;
+    string s;
+    cin>>N>>M;
+    for(int i=0;i<N;i++){
+        cin>>dogam1[i];
+        dogam2[i]={dogam1[i],i+1};
+    }
+    sort(dogam2,dogam2+N);
+    for(int i=0;i<M;i++){
+        cin>>s;
+        if(isdigit(s[0])){
+            cout<<dogam1[stoi(s)-1]<<'\n';
+        }else{
+            cout<<lower_bound(dogam2,dogam2+N,make_pair(s,0))->second<<'\n';
+        }
+    }
+    
+    return 0;
+}
