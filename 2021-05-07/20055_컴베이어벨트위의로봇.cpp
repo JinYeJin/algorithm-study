59a18df1efecd04053d04ed69510241794dac283
SangOk <sangok1993@naver.com>
2021-05-07 15:30:41 +0900

    Update 20055_컴베이어벨트위의로봇.cpp

--- 2021-05-07/20055_컴베이어벨트위의로봇.cpp
+++ 2021-05-07/20055_컴베이어벨트위의로봇.cpp
@@ -1,66 +1,48 @@
-#include "/usr/local/include/bits/stdc++.h"
+#include "bits/stdc++.h"
 
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(0);cin.tie(0);
-    FILE *stream =freopen("/Users/sangoklee/Documents/workspace/test/test/test.txt", "r", stdin);
-    if(!stream) perror("freopen");
-    int I,N,board[22][22],di[]={1,-1,0,0},dj[]={0,0,1,-1};
-    set<int> like[401];
     
-    memset(board,-1,sizeof(board));
-    cin>>N;
-    for(int i=1;i<=N;i++){
-        for(int j=1;j<=N;j++){
-            board[i][j]=0;
-        }
-    }
-    for(int n=0;n<N*N;n++){
-        cin>>I;
-        for(int d=0;d<4;d++){
-            int f;
-            cin>>f;
-            like[I].insert(f);
-        }
-        pair<int,int> max_like_empty={-1,-1};
-        int si,sj;
-        for(int i=1;i<=N;i++){
-            for(int j=1;j<=N;j++){
-                pair<int,int> like_empty={0,0};
-                if(board[i][j]==0){
-                    for(int d=0;d<4;d++){
-                        int ni=i+di[d],nj=j+dj[d];
-                        if(board[ni][nj]==0){
-                            like_empty.second++;
-                        }
-                        if(like[I].find(board[ni][nj])!=like[I].end()){//set 안에 있으면
-                            like_empty.first++;
-                        }
-                    }
-                    if(max_like_empty<like_empty){
-                        max_like_empty=like_empty;
-                        si=i, sj=j;
+    int N,K,ne;
+    deque<int> A;
+    cin>>N>>K;
+    deque<bool> robot(N,0);
+    
+    for(int i=0;i<2*N;i++){
+        cin>>ne;
+        A.push_back(ne);
+    }//1 2 ... 2N -> pop_back -> push_front
+    int cnt=0;
+    while(++cnt){
+        A.push_front(A.back());A.pop_back();//컨베이어 벨트 회전
+        robot.push_front(0);robot.pop_back();//컨베이어 벨트 위에있는 로봇도 같이 회전!
+        
+        robot.back()=0;
+        for(int i=N-2;i>=0;i--){//로봇이 움직여서 다음칸으로 이동
+            if(robot[i]==1 && robot[i+1]==0 && A[i+1]>0){
+                swap(robot[i],robot[i+1]);
+                if(--A[i+1]==0){
+                    if(--K==0){
+                        cout<<cnt;
+                        return 0;
                     }
                 }
             }
         }
-        board[si][sj]=I;
-    }
-    int answer=0,score[]={0,1,10,100,1000};
-    
-    for(int i=1;i<=N;i++){
-        for(int j=1;j<=N;j++){
-            int cnt=0;
-            for(int d=0;d<4;d++){
-                int ni=i+di[d],nj=j+dj[d];
-                if(like[board[i][j]].find(board[ni][nj])!=like[board[i][j]].end()) cnt++;
+        if(A.front()>0){//올리는 칸이 0보다 크면 로봇을 올리다.
+            robot.front()=1;
+            if(--A.front()==0){
+                if(--K==0){
+                    cout<<cnt;
+                    return 0;
+                }
             }
-            answer+=score[cnt];
         }
     }
-    cout<<answer;
+    
     return 0;
 }
 
