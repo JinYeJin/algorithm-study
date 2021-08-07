cd6da73d03b58aae785291fea34797fee4bcc6d9
진예진 <yeen0606@gmail.com>
2021-08-07 11:18:59 +0900

    Create 2469_진예진_사다리타기_미완.py

---
+++ 2021-08-07/2469_진예진_사다리타기_미완.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open("input/2469.txt")
+
+K = int(input())
+N = int(input())
+
+arrive = list(map(int, input().split()))
+
+ladder = []
+
+for n in range(N):
+    ladder.append(list(map(int, input.split())))
+
+
+def combination(num_bar, index):
+    pass
+
+
+for n in range(N):
+    으악
