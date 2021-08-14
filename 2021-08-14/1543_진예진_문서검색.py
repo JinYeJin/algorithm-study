71a31406b84175206125d21f4ba9ea2991d55af2
진예진 <yeen0606@gmail.com>
2021-08-14 11:17:28 +0900

    Update 1543_진예진_문서검색.py

--- 2021-08-14/1543_진예진_문서검색.py
+++ 2021-08-14/1543_진예진_문서검색.py
@@ -1,1 +1,31 @@
+import sys
+sys.stdin = open("input/1543.txt")
 
+doc = input()
+query = input()
+dlen = len(doc)
+qlen = len(query)
+
+count = 0
+start = 0
+limit = dlen - qlen
+
+# print(True if "a b " == "a b" else False)
+
+while start <= limit:
+# for index in range(dlen - qlen):
+    end = start+qlen
+    # print(doc[start:end])
+    # print("start", start)
+    # break
+    if doc[start:end] == query:
+        # print("same", doc[start:end], query)
+        count += 1
+        start = end
+        # print("start", start, "end", end)
+    else:
+        start += 1
+        # print("not", start)
+    # print()
+
+print(count)
